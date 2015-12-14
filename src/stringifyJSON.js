1b637f9c9905cdb236508dd0d2099ade1fcc83d2
Gerrit <gerrityntema@gmail.com>
2015-12-14 04:03:09 -0800

    Correctly stringify all except regular objects and functions

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,25 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	console.log(typeof obj)
+	var str = '';
+	if(obj === undefined) {
+		str += '';
+	} else if(typeof obj === 'number' || typeof obj === 'boolean' || !obj) {
+		str += "" + obj + "";	
+	} else if (typeof obj === 'string') {
+		str += "\"" + obj + "\"";
+	} else if(Array.isArray(obj)) {
+		str += '[';
+		for(var i = 0; i < obj.length; i++) {
+			str += stringifyJSON(obj[i]);
+			if(i < obj.length - 1) {
+				str += ',';
+			}
+		}
+		str += ']';
+	} else {
+		str += obj;
+	}
+	return str;
 };
