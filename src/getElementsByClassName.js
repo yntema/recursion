5b91568ed8a660dd279acd29f6e6781bc639a0f4
Gerrit <gerrityntema@gmail.com>
2015-12-14 00:40:06 -0800

    Create getElemntByClassName to pass specs

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+	var elements = [];	
+	var nodes = document.childNodes;
+	function searchNodes(nodeArray) {
+	  for(var i = 0; i < nodeArray.length; i++) {
+	  	if(nodeArray[i].classList) {
+		  	for(var j = 0; j < nodeArray[i].classList.length; j++) {
+		  		console.log("this class is: ", nodeArray[i].classList[j])
+			  	if(nodeArray[i].classList[j] === className) {
+			  	  elements.push(nodeArray[i])
+			  	}
+			  }	  	
+			}
+			if(nodeArray[i].childNodes[0] !== undefined && typeof nodeArray[i].childNodes === "object") {
+				searchNodes(nodeArray[i].childNodes)
+			}
+		}
+	}
+	searchNodes(nodes);
+	return elements;	
 };
